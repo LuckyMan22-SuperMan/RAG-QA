ab13706cb6f35e0a182c651d47c5ec1466bbd2b3
Lakshya Agrawal <lakshyaus4@gmail.com>
2026-07-31 16:50:13 +0530

    setup api

---
+++ static/app.js
@@ -0,0 +1,8 @@
+const $ = (id) => document.getElementById(id);
+
+async function api(path, opts) {
+  const res = await fetch(path, opts);
+  const data = await res.json().catch(() => ({}));
+  if (!res.ok) throw new Error(data.detail || `Request failed (${res.status})`);
+  return data;
+}
