161903c2bed5be0565d33e93b965e3ca689c2867
Lakshya Agrawal <lakshyaus4@gmail.com>
2026-08-03 19:45:57 +0530

    fn to highlight main search words in response

--- static/app.js
+++ static/app.js
@@ -11,3 +11,13 @@ function escapeHtml(s) {
   return s.replace(/[&<>"']/g, (c) =>
     ({ "&": "&amp;", "<": "&lt;", ">": "&gt;", '"': "&quot;", "'": "&#39;" }[c]));
 }
+
+function highlight(text, keywords) {
+  let out = escapeHtml(text);
+  for (const kw of keywords) {
+    if (kw.length < 2) continue;
+    const re = new RegExp(`\\b(${kw.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")})`, "gi");
+    out = out.replace(re, "<mark>$1</mark>");
+  }
+  return out;
+}
